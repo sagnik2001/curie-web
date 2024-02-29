eb4a170e3b9a327df1b75b220d9899c00bd1af99
Abhirup <abhirup@even.in>
2024-02-29 21:28:00 +0530

    Add banner in desktop.

---
+++ components/features/Landing/Desktop/IntroductionBanner/index.tsx
@@ -0,0 +1,62 @@
+import clsx from "clsx";
+import * as styles from "./IntroductionBanner.module.scss";
+import { calson } from "@utilities/font";
+
+export default function IntroductionBanner() {
+  return (
+    <div className={styles.container}>
+      <div className={clsx(styles.baseBand, styles.introducing)}>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+        <span className={calson.className}>introducing</span>
+      </div>
+      <div className={clsx(styles.baseBand, styles.india)}>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+        <span className={calson.className}>India's first</span>
+      </div>
+    </div>
+  );
+}
--- components/features/Landing/Desktop/index.tsx
+++ components/features/Landing/Desktop/index.tsx
@@ -1,11 +1,15 @@
 import Navbar from "@molecules/Navbar";
 import Hero from "./Hero";
+import Footer from "@molecules/Footer";
+import IntroductionBanner from "./IntroductionBanner";
 
 export default function DesktopLanding() {
   return (
     <>
       <Navbar />
       <Hero />
+      <IntroductionBanner />
+      <Footer landingPage />
     </>
   );
 }
