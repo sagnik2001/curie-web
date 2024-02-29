5d8a123c13d87281f0b7923521fb1165e6f1a6b1
Abhirup <abhirup@even.in>
2024-02-29 13:57:43 +0530

    Add explain.

--- components/features/Landing/Explain/index.tsx
+++ components/features/Landing/Explain/index.tsx
@@ -1,8 +1,17 @@
+import { animated, useSpring } from "@react-spring/web";
 import * as styles from "./Explain.module.scss";
+import Ornament1 from "@svgs/explain/ornament1.svg";
 
 export default function Explain() {
+  const AnimatedOrnament1 = animated(Ornament1);
+  // const [springValues,api] = useSpring(()=>({
+  //   top: {top: "43%",right: "42%"},
+  //   from: {top: "44%", right: "36%"},
+
+  // })) 
   return (
     <div className={styles.container}>
+      <img src="/bg-explain.png" alt="" className={styles.bg} />
       <h2 className={styles.title}>
         <span>High yield</span>
         <span>savings account</span>
@@ -11,6 +20,8 @@ export default function Explain() {
         <span>Zero balance account with</span>
         <span>no hidden fees</span>
       </p>
+      <AnimatedOrnament1 style={{ position: "absolute" }} />
+      <img src="/mobile.png" alt="" className={styles.img} />
     </div>
   );
 }
