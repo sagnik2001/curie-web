df4b70953407c986985b54a4965425fd57691e21
Abhirup <abhirup@even.in>
2024-03-01 23:05:00 +0530

    Add remaining featurws.

--- components/features/Landing/Desktop/Features/HassleFree/index.tsx
+++ components/features/Landing/Desktop/Features/HassleFree/index.tsx
@@ -1,5 +1,16 @@
 import * as styles from "./HassleFree.module.scss";
 
 export default function HassleFree() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <img src="/hasslefree-desktop.png" alt="" className={styles.bg} />
+      <div style={{ marginRight: "200px" }}>
+        <h2 className={styles.title}>
+          <span>Use your money</span>
+          <span>hassle free</span>
+        </h2>
+        <p className={styles.description}>with UPI & debit card</p>
+      </div>
+    </div>
+  );
 }
--- components/features/Landing/Desktop/Features/Invested/index.tsx
+++ components/features/Landing/Desktop/Features/Invested/index.tsx
@@ -3,6 +3,13 @@ import * as styles from "./Invested.module.scss";
 export default function Invested() {
   return (
     <div className={styles.container}>
+      <img src="/desktop/bg-invested.png" alt="" className={styles.bg} />
+      <h2 className={styles.title}>
+        <span>Use your money</span>
+        <span>hassle free</span>
+      </h2>
+      <p className={styles.description}>with UPI & debit card</p>
+      <img src="/desktop/img-invested.png" alt="" className={styles.img} />
     </div>
   );
 }
