ab2627cbcb8a9052bad7cb056c049539e7956874
Abhirup <abhirup@even.in>
2024-03-14 23:05:23 +0530

    Desktop hero and footer sections.

--- components/features/Landing/Desktop/Hero/index.tsx
+++ components/features/Landing/Desktop/Hero/index.tsx
@@ -10,21 +10,79 @@ import Ornament5 from "@svgs/desktop-hero/ornament5.svg";
 import Ornament6 from "@svgs/desktop-hero/ornament6.svg";
 import Ornament7 from "@svgs/desktop-hero/ornament7.svg";
 import Ornament8 from "@svgs/desktop-hero/ornament8.svg";
+import { useEffect, useState } from "react";
 
 export default function Hero() {
+  const [loadAnimation, setLoadAnimation] = useState(false);
+  useEffect(() => {
+    setLoadAnimation(true);
+  }, []);
   return (
-    <div style={{ position: "relative" }}>
-      <Ornament1 className={styles.ornament1} />
+    <div
+      style={{ position: "relative" }}
+      onClick={() => setLoadAnimation((an) => !an)}
+    >
+      <Ornament1
+        className={clsx(
+          styles.ornament1,
+          loadAnimation && styles.float,
+          loadAnimation && styles.animateOrnament1
+        )}
+      />
       <div className={styles.container}>
         <img src="/desktop/bg-hero.png" alt="" className={styles.bg} />
-        <Ornament2 className={styles.ornament2} />
-        <Ornament3 className={styles.ornament3} />
-        <Ornament5 className={styles.ornament5} />
-        <Ornament4 className={styles.ornament4} />
-        <Ornament6 className={styles.ornament6} />
-        <Ornament7 className={styles.ornament7} />
-        <Ornament8 className={styles.ornament8} />
-        <div>
+        <Ornament2
+          className={clsx(
+            styles.ornament2,
+            loadAnimation && styles.float,
+            loadAnimation && styles.animateOrnament2
+          )}
+        />
+        <Ornament3
+          className={clsx(
+            styles.ornament3,
+            loadAnimation && styles.float,
+            loadAnimation && styles.animateOrnament3
+          )}
+        />
+        <Ornament5
+          className={clsx(
+            styles.ornament5,
+            loadAnimation && styles.float,
+            loadAnimation && styles.animateOrnament5
+          )}
+        />
+        <Ornament4
+          className={clsx(
+            styles.ornament4,
+            loadAnimation && styles.float,
+            loadAnimation && styles.animateOrnament4
+          )}
+        />
+        <Ornament6
+          className={clsx(
+            styles.ornament6,
+            loadAnimation && styles.float,
+            loadAnimation && styles.animateOrnament6
+          )}
+        />
+        <Ornament7
+          className={clsx(
+            styles.ornament7,
+            loadAnimation && styles.float,
+            loadAnimation && styles.animateOrnament7
+          )}
+        />
+        <Ornament8
+          className={clsx(
+            styles.ornament8,
+            loadAnimation && styles.float,
+            loadAnimation && styles.animateOrnament8
+          )}
+        />
+        <div
+          className={clsx(styles.titleBox, loadAnimation && styles.animatedBox)}
+        >
           <h1 className={styles.title}>
             <span>Spend</span>
             <span>while your</span>
