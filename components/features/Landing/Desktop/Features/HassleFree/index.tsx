21dd8a77abd5e2b52dd3873890cedfac6f0e3b35
Abhirup <abhirup@even.in>
2024-03-21 21:10:10 +0530

    Complete safe section.

--- components/features/Landing/Desktop/Features/HassleFree/index.tsx
+++ components/features/Landing/Desktop/Features/HassleFree/index.tsx
@@ -1,10 +1,20 @@
 import * as styles from "./HassleFree.module.scss";
+import Ornament1 from "@svgs/hasslefree-desktop/ornament1.svg";
+import Ornament2 from "@svgs/hasslefree-desktop/ornament2.svg";
+import Ornament3 from "@svgs/hasslefree-desktop/ornament3.svg";
+import Ornament4 from "@svgs/hasslefree-desktop/ornament4.svg";
+import Ornament5 from "@svgs/hasslefree-desktop/ornament5.svg";
+import Ornament6 from "@svgs/hasslefree-desktop/ornament6.svg";
+import Ornament7 from "@svgs/hasslefree-desktop/ornament7.svg";
+import Ornament8 from "@svgs/hasslefree-desktop/ornament8.svg";
+import Ornament9 from "@svgs/hasslefree-desktop/ornament9.svg";
+import Ornament10 from "@svgs/hasslefree-desktop/ornament10.svg";
 
 export default function HassleFree() {
   return (
     <div className={styles.box}>
       <div className={styles.container}>
-        <img src="/hasslefree-desktop.png" alt="" className={styles.bg} />
+        <img src="/desktop/img-hasslefree.png" alt="" className={styles.img} />
         <div style={{ marginRight: "200px" }}>
           <h2 className={styles.title}>
             <span>Use your money</span>
@@ -13,6 +23,7 @@ export default function HassleFree() {
           <p className={styles.description}>with UPI & debit card</p>
         </div>
       </div>
+      <img src="/desktop/bg-hasslefree.png" alt="" className={styles.bg} />
     </div>
   );
 }
