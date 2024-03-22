617c91db66fd9c954e80751eebaef15786800c21
Abhirup <abhirup@even.in>
2024-03-22 23:44:43 +0530

    Add lottie animation.

--- components/features/Landing/DownloadMobile/index.tsx
+++ components/features/Landing/DownloadMobile/index.tsx
@@ -1,17 +1,27 @@
 import clsx from "clsx";
 import * as styles from "./DownloadMobile.module.scss";
 import { calson } from "@utilities/font";
+import Lottie from "lottie-react";
+import { useEffect, useState } from "react";
 
 export default function DownloadMobile() {
+  const [animationData, setAnimationData] = useState(null);
+  useEffect(() => {
+    fetch("/cta.json")
+      .then((response) => response.json())
+      .then((data) => setAnimationData(data));
+  }, []);
+
   return (
     <div className={styles.btn} onClick={() => {}}>
-      <a
+      {/* <a
         href="https://play.google.com/store/apps/details?id=com.yield.curie_money"
         target="_blank"
         className={calson.className}
       >
         <span>download curie</span>
-      </a>
+      </a> */}
+      <Lottie animationData={animationData} autoplay loop />
     </div>
   );
 }
