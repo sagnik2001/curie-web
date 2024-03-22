e8c417a0688caa5746606a59bd7e4db310e81e09
Abhirup <abhirup@even.in>
2024-03-22 22:09:48 +0530

    Add animation for the safe container.

--- components/features/Landing/Desktop/Features/Safe/index.tsx
+++ components/features/Landing/Desktop/Features/Safe/index.tsx
@@ -1,26 +1,75 @@
+import useOnScreen from "@hooks/useOnScreen";
 import * as styles from "./Safe.module.scss";
 import Ornament1 from "@svgs/safe-desktop/ornament1.svg";
 import Ornament2 from "@svgs/safe-desktop/ornament2.svg";
 import Ornament3 from "@svgs/safe-desktop/ornament3.svg";
 import Ornament4 from "@svgs/safe-desktop/ornament4.svg";
 import Ornament5 from "@svgs/safe-desktop/ornament5.svg";
+import { useRef } from "react";
+import clsx from "clsx";
 
 export default function Safe() {
+  const ref = useRef<HTMLDivElement>(null);
+  const isOnScreen = useOnScreen({
+    ref,
+    threshold: 0.75,
+    dontUpdateAfterIntersection: true,
+  });
   return (
-    <div className={styles.box}>
+    <div className={styles.box} ref={ref}>
       <div className={styles.container}>
-        <img src="/desktop/bg-save.png" alt="" className={styles.bg} />
-        <Ornament1 className={styles.ornament1} />
-        <Ornament2 className={styles.ornament2} />
-        <Ornament3 className={styles.ornament3} />
-        <Ornament4 className={styles.ornament4} />
-        <Ornament5 className={styles.ornament5} />
-        <h2 className={styles.title}>
+        <img
+          src="/desktop/bg-save.png"
+          alt=""
+          className={clsx(styles.bg, isOnScreen && styles.finalBg)}
+        />
+        <Ornament1
+          className={clsx(
+            styles.ornament1,
+            isOnScreen && styles.finalOrnament1
+          )}
+        />
+        <Ornament2
+          className={clsx(
+            styles.ornament2,
+            isOnScreen && styles.finalOrnament2
+          )}
+        />
+        <Ornament3
+          className={clsx(
+            styles.ornament3,
+            isOnScreen && styles.finalOrnament3
+          )}
+        />
+        <Ornament4
+          className={clsx(
+            styles.ornament4,
+            isOnScreen && styles.finalOrnament4
+          )}
+        />
+        <Ornament5
+          className={clsx(
+            styles.ornament5,
+            isOnScreen && styles.finalOrnament5
+          )}
+        />
+        <h2 className={clsx(styles.title, isOnScreen && styles.finalTitle)}>
           <span>Your money is</span>
           <span>always safe</span>
         </h2>
-        <p className={styles.description}>with our licensed Bank and AMC</p>
-        <img src="/desktop/img-save.png" alt="" className={styles.img} />
+        <p
+          className={clsx(
+            styles.description,
+            isOnScreen && styles.finalDescription
+          )}
+        >
+          with our licensed Bank and AMC
+        </p>
+        <img
+          src="/desktop/img-save.png"
+          alt=""
+          className={clsx(styles.img, isOnScreen && styles.finalImg)}
+        />
       </div>
     </div>
   );
