0eefc673b2b7bf119d69d5bc5645701982c4a84e
Abhirup <abhirup@even.in>
2024-03-23 14:06:02 +0530

    Hassle free section.

--- components/features/Landing/Desktop/Features/HassleFree/index.tsx
+++ components/features/Landing/Desktop/Features/HassleFree/index.tsx
@@ -8,14 +8,22 @@ import Ornament6 from "@svgs/hasslefree-desktop/ornament6.svg";
 import Ornament7 from "@svgs/hasslefree-desktop/ornament7.svg";
 import Ornament8 from "@svgs/hasslefree-desktop/ornament8.svg";
 import Ornament9 from "@svgs/hasslefree-desktop/ornament9.svg";
-import Ornament10 from "@svgs/hasslefree-desktop/ornament10.svg";
 
 export default function HassleFree() {
   return (
     <div className={styles.box}>
       <div className={styles.container}>
         <img src="/desktop/img-hasslefree.png" alt="" className={styles.img} />
-        <div style={{ marginRight: "200px" }}>
+        <Ornament1 className={styles.ornament1} />
+        <Ornament2 className={styles.ornament2} />
+        <Ornament3 className={styles.ornament3} />
+        <Ornament4 className={styles.ornament4} />
+        <Ornament5 className={styles.ornament5} />
+        <Ornament6 className={styles.ornament6} />
+        <Ornament7 className={styles.ornament7} />
+        <Ornament8 className={styles.ornament8} />
+        <Ornament9 className={styles.ornament9} />
+        <div className={styles.contentBox}>
           <h2 className={styles.title}>
             <span>Use your money</span>
             <span>hassle free</span>
