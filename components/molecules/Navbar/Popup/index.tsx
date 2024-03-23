c92f4aa4fcd48fa9ce84a6b6fc6241fcd8978b70
Abhirup <abhirup@even.in>
2024-03-23 15:55:33 +0530

    Add download pop-up on desktop.

---
+++ components/molecules/Navbar/Popup/index.tsx
@@ -0,0 +1,37 @@
+import { createPortal } from "react-dom";
+import * as styles from "./Popup.module.scss";
+import clsx from "clsx";
+import CloseSvg from "@svgs/close.svg";
+import { figtree } from "@utilities/font";
+
+function Download({ onClose }: { onClose: () => void }) {
+  return (
+    <>
+      <div className={styles.bg} />
+      <div className={styles.elementBox}>
+        <div className={clsx(styles.modal, figtree.className)}>
+          <div className={styles.close} onClick={onClose}>
+            <CloseSvg />
+          </div>
+          <div className={styles.contentBox}>
+            <div className={styles.imgBox}>
+              <img src="/play-logo.png" alt="" />
+              <img src="/apple-logo.png" alt="" />
+            </div>
+            <h2 className={styles.title}>Scan this QR code</h2>
+            <ul className={styles.para}>
+              <li>Scan the QR code using your phone camera</li>
+              <li>Click on the link generated to download Curie</li>
+            </ul>
+          </div>
+          <img src="/download-qr.png" alt="" className={styles.img} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default function Popup({ onClose }: { onClose: () => void }) {
+  const element = document.getElementById("portal");
+  return createPortal(<Download onClose={onClose} />, element!);
+}
--- components/molecules/Navbar/index.tsx
+++ components/molecules/Navbar/index.tsx
@@ -2,32 +2,34 @@ import clsx from "clsx";
 import * as styles from "./Navbar.module.scss";
 import CurieLogo from "@svgs/curie-logo.svg";
 import { calson } from "@utilities/font";
+import { useState } from "react";
+import Popup from "./Popup";
 
 export default function Navbar({ className = "" }: { className?: string }) {
+  const [showPopup, setShowPopup] = useState(false);
   return (
-    <div className={clsx(styles.container, className)}>
-      <div className={styles.box}>
-        <div
-          onClick={() =>
-            window.scroll({
-              top: 0,
-              left: 0,
-              behavior: "smooth",
-            })
-          }
-        >
-          <CurieLogo />
-        </div>
-        <div className={styles.btn}>
-          <a
-            href="https://play.google.com/store/apps/details?id=com.yield.curie_money"
-            target="_blank"
-            className={calson.className}
+    <>
+      <div className={clsx(styles.container, className)}>
+        <div className={styles.box}>
+          <div
+            onClick={() =>
+              window.scroll({
+                top: 0,
+                left: 0,
+                behavior: "smooth",
+              })
+            }
           >
-            <span>download curie</span>
-          </a>
+            <CurieLogo />
+          </div>
+          <div className={styles.btn} onClick={() => setShowPopup(true)}>
+            <a className={calson.className}>
+              <span>download curie</span>
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+      {showPopup && <Popup onClose={() => setShowPopup(false)} />}
+    </>
   );
 }
