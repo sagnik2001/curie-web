7ca024f5c3ab1f309a975912720d27ca98398a84
Abhirup <abhirup@even.in>
2024-03-23 17:19:47 +0530

    Animate hasslefree section.

--- components/features/Landing/Desktop/Features/HassleFree/index.tsx
+++ components/features/Landing/Desktop/Features/HassleFree/index.tsx
@@ -1,3 +1,4 @@
+import useOnScreen from "@hooks/useOnScreen";
 import * as styles from "./HassleFree.module.scss";
 import Ornament1 from "@svgs/hasslefree-desktop/ornament1.svg";
 import Ornament2 from "@svgs/hasslefree-desktop/ornament2.svg";
@@ -8,30 +9,114 @@ import Ornament6 from "@svgs/hasslefree-desktop/ornament6.svg";
 import Ornament7 from "@svgs/hasslefree-desktop/ornament7.svg";
 import Ornament8 from "@svgs/hasslefree-desktop/ornament8.svg";
 import Ornament9 from "@svgs/hasslefree-desktop/ornament9.svg";
+import { useRef, useState, useEffect } from "react";
+import clsx from "clsx";
 
 export default function HassleFree() {
+  const ref = useRef<HTMLDivElement>(null);
+  const [floatOrnaments, setFloatOrnamanents] = useState(false);
+  const isOnScreen = useOnScreen({
+    ref,
+    threshold: 0.5,
+    dontUpdateAfterIntersection: true,
+  });
+  useEffect(() => {
+    if (!isOnScreen || floatOrnaments) return;
+    setTimeout(() => {
+      setFloatOrnamanents(true);
+    }, 800);
+  }, [isOnScreen, floatOrnaments]);
   return (
-    <div className={styles.box}>
+    <div className={styles.box} ref={ref}>
       <div className={styles.container}>
-        <img src="/desktop/img-hasslefree.png" alt="" className={styles.img} />
-        <Ornament1 className={styles.ornament1} />
-        <Ornament2 className={styles.ornament2} />
-        <Ornament3 className={styles.ornament3} />
-        <Ornament4 className={styles.ornament4} />
-        <Ornament5 className={styles.ornament5} />
-        <Ornament6 className={styles.ornament6} />
-        <Ornament7 className={styles.ornament7} />
-        <Ornament8 className={styles.ornament8} />
-        <Ornament9 className={styles.ornament9} />
+        <img
+          src="/desktop/img-hasslefree.png"
+          alt=""
+          className={clsx(styles.img, isOnScreen && styles.finalImg)}
+        />
+        <Ornament1
+          className={clsx(
+            styles.ornament1,
+            isOnScreen && styles.finalOrnament1,
+            floatOrnaments && styles.float
+          )}
+        />
+        <Ornament2
+          className={clsx(
+            styles.ornament2,
+            isOnScreen && styles.finalOrnament1,
+            floatOrnaments && styles.float
+          )}
+        />
+        <Ornament3
+          className={clsx(
+            styles.ornament3,
+            isOnScreen && styles.finalOrnament1,
+            floatOrnaments && styles.float
+          )}
+        />
+        <Ornament4
+          className={clsx(
+            styles.ornament4,
+            isOnScreen && styles.finalOrnament1,
+            floatOrnaments && styles.float
+          )}
+        />
+        <Ornament5
+          className={clsx(
+            styles.ornament5,
+            isOnScreen && styles.finalOrnament1,
+            floatOrnaments && styles.float
+          )}
+        />
+        <Ornament6
+          className={clsx(
+            styles.ornament6,
+            isOnScreen && styles.finalOrnament1,
+            floatOrnaments && styles.float
+          )}
+        />
+        <Ornament7
+          className={clsx(
+            styles.ornament7,
+            isOnScreen && styles.finalOrnament1,
+            floatOrnaments && styles.float
+          )}
+        />
+        <Ornament8
+          className={clsx(
+            styles.ornament8,
+            isOnScreen && styles.finalOrnament1,
+            floatOrnaments && styles.float
+          )}
+        />
+        <Ornament9
+          className={clsx(
+            styles.ornament9,
+            isOnScreen && styles.finalOrnament1,
+            floatOrnaments && styles.float
+          )}
+        />
         <div className={styles.contentBox}>
-          <h2 className={styles.title}>
+          <h2 className={clsx(styles.title, isOnScreen && styles.finalTitle)}>
             <span>Use your money</span>
             <span>hassle free</span>
           </h2>
-          <p className={styles.description}>with UPI & debit card</p>
+          <p
+            className={clsx(
+              styles.description,
+              isOnScreen && styles.finalDescription
+            )}
+          >
+            with UPI & debit card
+          </p>
         </div>
       </div>
-      <img src="/desktop/bg-hasslefree.png" alt="" className={styles.bg} />
+      <img
+        src="/desktop/bg-hasslefree.png"
+        alt=""
+        className={clsx(styles.bg, isOnScreen && styles.finalBg)}
+      />
     </div>
   );
 }
