feb1e9c85754695ce8d000c10b2b1a9d9a48085b
Abhirup <abhirup@even.in>
2024-03-25 22:49:39 +0530

    Use pause animation.

--- components/features/Landing/Desktop/Hero/index.tsx
+++ components/features/Landing/Desktop/Hero/index.tsx
@@ -10,10 +10,34 @@ import Ornament5 from "@svgs/desktop-hero/ornament5.svg";
 import Ornament6 from "@svgs/desktop-hero/ornament6.svg";
 import Ornament7 from "@svgs/desktop-hero/ornament7.svg";
 import Ornament8 from "@svgs/desktop-hero/ornament8.svg";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
+import usePauseAnimation from "@hooks/usePauseAnimation";
 
 export default function Hero() {
   const [loadAnimation, setLoadAnimation] = useState(false);
+  const ornament1Ref = useRef<HTMLElement>(null);
+  const ornament2Ref = useRef<HTMLElement>(null);
+  const ornament3Ref = useRef<HTMLElement>(null);
+  const ornament4Ref = useRef<HTMLElement>(null);
+  const ornament5Ref = useRef<HTMLElement>(null);
+  const ornament6Ref = useRef<HTMLElement>(null);
+  const ornament7Ref = useRef<HTMLElement>(null);
+  const ornament8Ref = useRef<HTMLElement>(null);
+  const bgRef = useRef<HTMLSourceElement>(null);
+  const textRef = useRef<HTMLHeadingElement>(null);
+  const paraRef = useRef<HTMLParagraphElement>(null);
+  usePauseAnimation(ornament1Ref);
+  usePauseAnimation(ornament2Ref);
+  usePauseAnimation(ornament3Ref);
+  usePauseAnimation(ornament4Ref);
+  usePauseAnimation(ornament5Ref);
+  usePauseAnimation(ornament6Ref);
+  usePauseAnimation(ornament7Ref);
+  usePauseAnimation(ornament8Ref);
+  usePauseAnimation(bgRef);
+  usePauseAnimation(textRef);
+  usePauseAnimation(paraRef);
+
   useEffect(() => {
     setTimeout(() => {
       setLoadAnimation(true);
@@ -27,6 +51,7 @@ export default function Hero() {
           loadAnimation && styles.float,
           loadAnimation && styles.animateOrnament1
         )}
+        ref={ornament1Ref}
       />
       <div className={styles.container}>
         <picture>
@@ -34,6 +59,7 @@ export default function Hero() {
             srcSet="/desktop/bg-hero.avif"
             type="image/avif"
             className={styles.bg}
+            ref={bgRef}
           />
           <source
             srcSet="/desktop/bg-hero.webp"
@@ -53,6 +79,7 @@ export default function Hero() {
             loadAnimation && styles.float,
             loadAnimation && styles.animateOrnament2
           )}
+          ref={ornament2Ref}
         />
         <Ornament3
           className={clsx(
@@ -60,6 +87,7 @@ export default function Hero() {
             loadAnimation && styles.float,
             loadAnimation && styles.animateOrnament3
           )}
+          ref={ornament3Ref}
         />
         <Ornament5
           className={clsx(
@@ -67,6 +95,7 @@ export default function Hero() {
             loadAnimation && styles.float,
             loadAnimation && styles.animateOrnament5
           )}
+          ref={ornament5Ref}
         />
         <Ornament4
           className={clsx(
@@ -74,6 +103,7 @@ export default function Hero() {
             loadAnimation && styles.float,
             loadAnimation && styles.animateOrnament4
           )}
+          ref={ornament4Ref}
         />
         <Ornament6
           className={clsx(
@@ -81,6 +111,7 @@ export default function Hero() {
             loadAnimation && styles.float,
             loadAnimation && styles.animateOrnament6
           )}
+          ref={ornament6Ref}
         />
         <Ornament7
           className={clsx(
@@ -88,6 +119,7 @@ export default function Hero() {
             loadAnimation && styles.float,
             loadAnimation && styles.animateOrnament7
           )}
+          ref={ornament7Ref}
         />
         <Ornament8
           className={clsx(
@@ -95,15 +127,18 @@ export default function Hero() {
             loadAnimation && styles.float,
             loadAnimation && styles.animateOrnament8
           )}
+          ref={ornament8Ref}
         />
         <div
           className={clsx(styles.titleBox, loadAnimation && styles.animatedBox)}
         >
-          <h1 className={styles.title}>
+          <h1 className={styles.title} ref={textRef}>
             <span>Spend</span>
             <span>while your</span>
           </h1>
-          <p className={styles.para}>money multiplies</p>
+          <p className={styles.para} ref={paraRef}>
+            money multiplies
+          </p>
         </div>
         <div className={styles.scrollBtn}>
           <ScrollSvg />
