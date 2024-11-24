2ee62492bca5dca1f45c80353cf21d33b79d2a90
Sagnik Pal <palsagnik2001@gmail.com>
2024-11-24 13:43:43 +0530

    Set padding top

--- components/features/Landing/Desktop/Hero/IntroductionBanner/index.tsx
+++ components/features/Landing/Desktop/Hero/IntroductionBanner/index.tsx
@@ -1,23 +1,54 @@
 import clsx from "clsx";
 import * as styles from "./IntroductionBanner.module.scss";
-import { calson } from "@utilities/font";
 import Explain_Comp from "../../Explain_2";
+import { useEffect, useState } from "react";
 
 export default function IntroductionBanner() {
+  const [paddingTop, setPaddingTop] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const maxPadding = 150; // Maximum padding-top limit
+      const scrollFactor = 0.1; // 5px increase for every 10px scroll
+
+      // Calculate new padding-top based on scrollY
+      const newPadding = Math.min(window.scrollY * scrollFactor, maxPadding);
+
+      setPaddingTop(newPadding); // Update padding state
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const containerStyle = {
+    paddingTop: `${paddingTop}px`, // Dynamically adjust padding
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.animationContent}>
-      <img className={styles.headingImg} src="/desktop/curie_save.png" alt="curie-save"/>
-      <div className={styles.subHeading}>
-        <span>
-        The supercharged savings account
-        </span>
+    <div id="hero-section" className={styles.container} style={containerStyle}>
+      <div className={clsx(!paddingTop && styles.animationContent,
+          styles.normalContent
+      )}>
+        <img
+          className={styles.headingImg}
+          src="/desktop/curie_save.png"
+          alt="curie-save"
+        />
+        <div className={styles.subHeading}>
+          <span>The supercharged savings account</span>
         </div>
-        </div>
-        <div className={styles.ctnBg}>  
-          <img className={styles.curieGraph} src="/desktop/curie-graph.png" alt="curie-graph"/>
-          <Explain_Comp/>
-          </div>
+      </div>
+      <div className={styles.ctnBg}>
+        <img
+          className={styles.curieGraph}
+          src="/desktop/curie-graph.png"
+          alt="curie-graph"
+        />
+        <Explain_Comp />
+      </div>
     </div>
   );
 }
