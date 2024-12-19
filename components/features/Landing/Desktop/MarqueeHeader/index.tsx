c1ce5e1e07e59b573cfa040201b9320e7159d81c
Sagnik Pal <palsagnik2001@gmail.com>
2024-12-19 13:42:13 +0530

    Suggested changes on banner and faq

--- components/features/Landing/Desktop/MarqueeHeader/index.tsx
+++ components/features/Landing/Desktop/MarqueeHeader/index.tsx
@@ -1,10 +1,26 @@
 import { calson } from "@utilities/font"
 import styles from "./Marquee.module.scss"
-import { HorizontalTicker } from "react-infinite-ticker";
+import dynamic from "next/dynamic";
+import { ReactNode, useEffect, useState } from "react";
 
+interface HorizontalTickerProps {
+  children: ReactNode;
+  duration: number;
+}
 
+const HorizontalTicker = dynamic<HorizontalTickerProps>(() =>
+  import("react-infinite-ticker").then((mod) => mod.HorizontalTicker), { ssr: false });
 
-const MarqueeHeader = ({isMobile=""}) => {
+interface MarqueeHeaderProps {
+  isMobile?: string;
+}
+
+const MarqueeHeader : React.FC<MarqueeHeaderProps> = ({isMobile=""}) => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+      setIsClient(true); // Ensures this runs only in the browser
+  }, []);
     return (
         <div className={`${isMobile!=="mobile" ? styles.marqueeContainer : styles.mobileMarqueeContainer}`} >
     <div className={ `${isMobile!=='mobile' ? styles.marqueeContent : styles.mobileMarqueeContent} ${calson.className}`}>
@@ -12,7 +28,7 @@ const MarqueeHeader = ({isMobile=""}) => {
    <div className={`${isMobile==='mobile' ? styles.marqueeMobileInnerContent : styles.marqueeInnerContent}`}>
                   
               
-                <HorizontalTicker duration={100000}>
+                {isClient && <HorizontalTicker duration={100000}>
                 <span>We announced our funding!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
                     <span>We announced our funding!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
                     <span>We announced our funding!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
@@ -21,7 +37,7 @@ const MarqueeHeader = ({isMobile=""}) => {
                     <span>We announced our funding!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
                     <span>We announced our funding!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
                     <span>We announced our funding!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
-      </HorizontalTicker>
+      </HorizontalTicker>}
       </div>
     </div>
     
