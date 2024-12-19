4fbf363fe962cf839952c1ae2e42940662cc4cd2
Sagnik Pal <palsagnik2001@gmail.com>
2024-12-19 13:35:03 +0530

    Suggested changes on banner and faq

--- components/features/Landing/Desktop/MarqueeHeader/index.tsx
+++ components/features/Landing/Desktop/MarqueeHeader/index.tsx
@@ -1,15 +1,30 @@
 import { calson } from "@utilities/font"
 import styles from "./Marquee.module.scss"
+import { HorizontalTicker } from "react-infinite-ticker";
+
+
 
 const MarqueeHeader = ({isMobile=""}) => {
     return (
         <div className={`${isMobile!=="mobile" ? styles.marqueeContainer : styles.mobileMarqueeContainer}`} >
     <div className={ `${isMobile!=='mobile' ? styles.marqueeContent : styles.mobileMarqueeContent} ${calson.className}`}>
-    <span>We announced our funding!&nbsp;&nbsp;&nbsp;&nbsp;</span>
-  <span>We announced our funding!&nbsp;&nbsp;&nbsp;&nbsp;</span>
-  <span>We announced our funding!&nbsp;&nbsp;&nbsp;&nbsp;</span>
-  <span>We announced our funding!&nbsp;&nbsp;&nbsp;&nbsp;</span>
+ 
+   <div className={`${isMobile==='mobile' ? styles.marqueeMobileInnerContent : styles.marqueeInnerContent}`}>
+                  
+              
+                <HorizontalTicker duration={100000}>
+                <span>We announced our funding!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                    <span>We announced our funding!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                    <span>We announced our funding!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                    <span>We announced our funding!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                    <span>We announced our funding!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                    <span>We announced our funding!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                    <span>We announced our funding!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                    <span>We announced our funding!&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+      </HorizontalTicker>
+      </div>
     </div>
+    
   </div>
     )
 }
